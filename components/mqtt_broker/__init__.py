a9bdbb63a7962ffb2d5f7528a2bd99e5db4f81e8
KuhlaJusa <>
2025-05-28 19:03:31 +0200

    add broker idf component

--- components/mqtt_broker/__init__.py
+++ components/mqtt_broker/__init__.py
@@ -1,7 +1,19 @@
 import esphome.config_validation as cv
+from esphome.components.esp32 import add_idf_component
 import esphome.codegen as cg
 
 from esphome.const import CONF_ID
+from esphome.const import (
+    CONF_DISABLED,
+    CONF_ID,
+    CONF_PORT,
+    CONF_PROTOCOL,
+    CONF_SERVICE,
+    CONF_SERVICES,
+    KEY_CORE,
+    KEY_FRAMEWORK_VERSION,
+)
+from esphome.core import CORE, coroutine_with_priority
 
 
 mqtt_broker_ns = cg.esphome_ns.namespace("mqtt_broker")
@@ -13,6 +25,15 @@ CONFIG_SCHEMA = cv.Schema({
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
 
+    if CORE.using_esp_idf and CORE.data[KEY_CORE][KEY_FRAMEWORK_VERSION] >= cv.Version(
+        5, 1, 0
+    ):
+        add_idf_component(
+            name="mosquitto",
+            repo="https://github.com/espressif/esp-protocols.git",
+            ref="mosquitto-2.0.20~2",
+            path="components/mosquitto",
+        )
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
